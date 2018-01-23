577136cb36d8809c39a3e3c1e2061946c279c082
yaowenxi <yaowenxi@ppdai.com>
2018-01-23 17:29:59 +0800

    初始化

---
+++ src/main/java/cn/worthy/swagger/Swagger2.java
@@ -0,0 +1,13 @@
+package cn.worthy.swagger;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Author:yaowenxi
+ * Date: 2018/1/23
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+}
